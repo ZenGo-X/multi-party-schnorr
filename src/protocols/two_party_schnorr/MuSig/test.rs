b6629ead8bbc7048bfdae30f2f15c322530f9230
omer sh <omer.shlomovits@gmail.com>
2018-07-12 10:36:58 +0300

    test for 1 party signing

--- src/protocols/two_party_schnorr/MuSig/test.rs
+++ src/protocols/two_party_schnorr/MuSig/test.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod tests {
+    use ::BigInt;
     use ::EC;
     use ::protocols::two_party_schnorr::MuSig::party_one;
     use ::protocols::two_party_schnorr::MuSig::party_two;
@@ -110,62 +111,34 @@ mod tests {
     #[test]
 
     fn test_schnorr_one_party(){
-        
+        let ec_context = EC::new();
         let message: [u8; 4] = [79, 77, 69, 82];
         let party1_key = party_one::Party1KeyGen::key_gen(&ec_context);
-        let party2_key = party_two::Party2KeyGen::key_gen(&ec_context);
-
-        //generate R1 and send com(R1) to p2:
         let party1_ephemeral_key = party_one::Party1EphemeralKey::create_from_private_key(&ec_context, &party1_key, &message);
-        //generate R2 and send com(R2) to p1:
-        let party2_ephemeral_key = party_two::Party2EphemeralKey::create_from_private_key(&ec_context, &party2_key, &message);
-        // p1 release R1' and p2 test com(R1') = com(R1):
-        assert!(party_one::Party1EphemeralKey::test_party2_com(&party2_ephemeral_key.party2_ephemeral_public_key,
-                                                               &party2_ephemeral_key.blind_factor, &party2_ephemeral_key.party2_Eph_key_comm));
-        // p2 release R2' and p1 test com(R2') = com(R2):
-        assert!(party_two::Party2EphemeralKey::test_party1_com(&party1_ephemeral_key.party1_ephemeral_public_key,
-                                                               &party1_ephemeral_key.blind_factor, &party1_ephemeral_key.party1_Eph_key_comm));
-
-        // compute apk:
-        let party1_key_agg = party_one::Party1KeyAgg::key_aggregation(&ec_context,&party1_key.party1_public_key,
-                                                                      &party2_key.party2_public_key);
-        let party2_key_agg = party_two::Party2KeyAgg::key_aggregation(&ec_context,&party1_key.party1_public_key,
-                                                                      &party2_key.party2_public_key);
-        assert_eq!(party1_key_agg.apk, party2_key_agg.apk);
-
-        // compute R' = R1+R2:
-        let party1_Rtag = party_one::Party1EphemeralKey::add_ephemeral_pub_keys(&ec_context,&party1_ephemeral_key.party1_ephemeral_public_key,
-                                                                                &party2_ephemeral_key.party2_ephemeral_public_key);
-        let party2_Rtag = party_two::Party2EphemeralKey::add_ephemeral_pub_keys(&ec_context,&party1_ephemeral_key.party1_ephemeral_public_key,
-                                                                                &party2_ephemeral_key.party2_ephemeral_public_key);
-
-        assert_eq!(party1_Rtag, party2_Rtag);
 
         // compute c = H0(Rtag || apk || message)
-        let party1_H0 = party_one::Party1EphemeralKey::hash_0(&party1_Rtag, &party1_key_agg.apk, &message);
-        let party2_H0 = party_two::Party2EphemeralKey::hash_0(&party2_Rtag, &party2_key_agg.apk, &message);
-        assert_eq!(party1_H0, party2_H0);
+        let party1_H0 = party_one::Party1EphemeralKey::hash_0(&party1_ephemeral_key.party1_ephemeral_public_key,
+                                                              &party1_key.party1_public_key, &message);
 
         // compute partial signature s_i and send to the other party:
-        let s1 = party_one::Party1EphemeralKey::sign1(&party1_ephemeral_key, &party1_H0, &party1_key, &party1_key_agg.hash1_a1);
-        let s2 = party_two::Party2EphemeralKey::sign2(&party2_ephemeral_key, &party2_H0, &party2_key, &party2_key_agg.hash1_a2);
+        let s1 = party_one::Party1EphemeralKey::sign1(&party1_ephemeral_key, &party1_H0, &party1_key, &BigInt::from(1));
 
         // signature s:
-        let (R, s) = party_one::Party1EphemeralKey::add_signature_parts(&s1, &s2, &party1_Rtag);
+        let (R, s) = party_one::Party1EphemeralKey::add_signature_parts(&s1, &BigInt::from(0), &party1_ephemeral_key.party1_ephemeral_public_key);
 
         // verify:
-        assert!(party_one::Party1EphemeralKey::verify(&ec_context, &s, &R, &party1_key_agg.apk, &message).is_ok())
+        assert!(party_one::Party1EphemeralKey::verify(&ec_context, &s, &R, &party1_key.party1_public_key, &message).is_ok())
 
     }
 
-
+/*
     #[test]
 
     fn test_schnorr_bip(){
         let ec_context = EC::new();
         let private_key_raw =  "0000000000000000000000000000000000000000000000000000000000000001";
         let public_key_raw =  "0279BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798";
-        let message =  "0000000000000000000000000000000000000000000000000000000000000000"
+        let message =  "0000000000000000000000000000000000000000000000000000000000000000";
         //output Signature: 787A848E71043D280C50470E8E1532B2DD5D20EE912A45DBDD2BD1DFBF187EF67031A98831859DC34DFFEEDDA86831842CCD0079E1F92AF177F7F22CC1DCED05
 
         let message: [u8; 4] = [79, 77, 69, 82];
@@ -214,5 +187,5 @@ mod tests {
         assert!(party_one::Party1EphemeralKey::verify(&ec_context, &s, &R, &party1_key_agg.apk, &message).is_ok())
 
     }
-
+*/
 }
